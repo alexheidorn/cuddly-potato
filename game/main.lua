c218b001ad15d0ddfc5c866289e8eec36acf7747
Alex Heidorn <heido023@d.umn.edu>
2025-02-21 18:49:46 -0600

    draw line in middle of window

--- game/main.lua
+++ game/main.lua
@@ -10,6 +10,9 @@ function love.draw()
 	love.graphics.setColor(255, 0, 0, 128)
 	love.graphics.print("Hello world!", 100, 100)
 	love.graphics.line(0, 0, 400, 400)
+
+	love.graphics.setColor(0, 0, 255, 255)
+	love.graphics.line(0,  love.graphics.getHeight()/2, love.graphics.getWidth(), love.graphics.getHeight()/2)
 end
 
 function love.quit()
